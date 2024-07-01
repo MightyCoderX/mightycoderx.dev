6cd9f947789c04c41a858e27350623573919dc5a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-01 02:57:24 +0200

    Update skills

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,11 +1,10 @@
 import SkillCard from '/components/SkillCard.js';
 
+const skills = ['html', 'css', 'js', 'nodejs', 'python', 'vscode', 'git', 'linux', 'docker'];
+
 /**
  * @param {Document} content
  */
-
-const skills = ['html', 'css', 'js', 'nodejs', 'python', 'c', 'vscode', 'github'];
-
 export function init(content)
 {   
     /**@type {HTMLDivElement} */
